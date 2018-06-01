60055957425f2d189323a054862db712168bdbec
18955515189 <18955515189@163.com>
2018-06-02 01:46:40 +0800

    redis使用案例 create by david 2018-06-02

---
+++ src/main/java/com/redis/demo/JedisClientTest.java
@@ -0,0 +1,24 @@
+package com.redis.demo;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisClientTest {
+	
+	
+	public static void main(String[] args) {
+		
+		//创建一个jedis客户端对象（redis的客户端连接）
+		Jedis client = new Jedis("hadoop01", 6379);
+		
+		//测试服务器是否连通
+		String resp = client.ping();
+		
+		System.out.println(resp);
+		
+		
+	}
+	
+	
+	
+
+}
