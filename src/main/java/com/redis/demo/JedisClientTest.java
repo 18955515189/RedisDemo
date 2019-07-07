4190460c19eafefaa2c646c5c8f0193df4f8881e
david_zhou <18955515189@163.com>
2019-07-07 23:53:41 +0800

    测试Git222

--- src/main/java/com/redis/demo/JedisClientTest.java
+++ src/main/java/com/redis/demo/JedisClientTest.java
@@ -7,7 +7,7 @@ public class JedisClientTest {
 	
 	public static void main(String[] args) {
 		
-		//创建一个jedis客户端对象（redis的客户端连接）12321
+		//创建一个jedis客户端对象（redis的客户端连接）122121212
 		Jedis client = new Jedis("hadoop01", 6379);
 		
 		//测试服务器是否连通
